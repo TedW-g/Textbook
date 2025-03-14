80ea5368aa312a1a09e6429524f49f0249302791
TedW-g <tedw3@nycstudents.net>
2025-03-14 13:31:07 +0000

    commit

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main
 {
     public static void main (String[] args)
     {
-        
+        Textbook bio2015 = new Textbook("Biology", 49.75, 2);
+        Textbook bio2019 = new Textbook("Biology", 49.75, 2);
+        System.out.println(bio2019.getEdition());
     }
 }
--- Textbook.java
+++ Textbook.java
@@ -1,4 +1,12 @@
 public class Textbook extends Book
 {
+    private int edition;
 
+    public Textbook(String title, double price, int edition)
+    {
+        super(title, price);
+        this.edition = edition;
+    }
+
+    
 }
